b6affb3516cab9728d35c29468b2b0de1d926a38
mshahhat <mohammed.shahhat@gmail.com>
2017-04-20 15:09:09 -0500

    Second exercise.

--- MyApp/src/app/counter-component/counter-component.component.ts
+++ MyApp/src/app/counter-component/counter-component.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'counter',
   template: `
-    <button (click)="decrementCounter()"></button>
+    <button (click)="decrementCounter()">-</button>
     {{counterValue}}
-    <button (click)="incrementCounter()"></button>
+    <button (click)="incrementCounter()">+</button>
   `
 })
 export class CounterComponentComponent implements OnInit {
+  @Input() counter: number;
+  counterValue = 0;
+  @Output() counterChange = new EventEmitter();
 
-  @Input() counterValue: number;
-  constructor() { }
+  constructor() {
+  }
 
 
   ngOnInit() {
+    if (this.counter !== 0) {
+      this.counterValue = this.counter;
+    }
   }
 
   incrementCounter() {
     this.counterValue = this.counterValue - 1 + 2;
+    this.counterChange.emit(this.counterValue);
   }
 
   decrementCounter() {
     this.counterValue = this.counterValue - 1;
+    this.counterChange.emit(this.counterValue);
 
   }
 }
